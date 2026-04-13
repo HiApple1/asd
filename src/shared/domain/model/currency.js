fc838d8ee9e93b785b1897cf0f2eba6dd82b0cc0
alumnos <u202416552@upc.edu.pe>
2026-04-13 17:57:57 -0500

    refactor(errors):  fixed currency.js

--- src/shared/domain/model/currency.js
+++ src/shared/domain/model/currency.js
@@ -1,10 +1,12 @@
+import {ValidationError} from "./errors.js";
+
 class Currency {
     static #VALID_CODES = ['USD', 'EUR', 'GBP', 'jpy'];
     #code;
 
     constructor(code) {
         if (!Currency.#VALID_CODES.includes(code))
-            throw new Error(`Invalid code: ${code}. Must be ${Currency.#VALID_CODES}`);
+            throw new ValidationError(`Invalid code: ${code}. Must be ${Currency.#VALID_CODES}`);
         this.#code = code;
     }
 }
